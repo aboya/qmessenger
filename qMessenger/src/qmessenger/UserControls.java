011a658dca4d122d6d60489ac196c0db1c5e52eb
aboimov <aboimov@4c8b1924-dde1-11de-aa1f-bbd297b2055c>
2009-12-02 19:43:46 +0000

    Creating base Class for Controlls and redirectiong events via Base Class

--- qMessenger/src/qmessenger/UserControls.java
+++ qMessenger/src/qmessenger/UserControls.java
@@ -5,6 +5,9 @@
 
 package qmessenger;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
@@ -19,18 +22,24 @@ public class UserControls {
     OutputTextField outputTextField;
     ButtonSendMessage buttonSendMessage;
 
+    Vector<Controll> controlsList;
+
     public UserControls(Composite composite, Display display) {
+        controlsList = new Vector<Controll>();
         userList = new UserList(composite, display);
+        controlsList.add(userList);
         inputTextField = new InputTextField(composite);
+        controlsList.add(inputTextField);
         outputTextField = new OutputTextField(composite);
+        controlsList.add(outputTextField);
         buttonSendMessage = new ButtonSendMessage(composite);
+        controlsList.add(buttonSendMessage);
     }
     public void Resize(Rectangle rect)
     {
-        userList.Resize(rect);
-        inputTextField.Resize(rect);
-        outputTextField.Resize(rect);
-        buttonSendMessage.Resize(rect);
+        int i, n  = controlsList.size();
+        for(i = 0; i < n; i++)
+            controlsList.get(i).Resize(rect);
     }
 
 
